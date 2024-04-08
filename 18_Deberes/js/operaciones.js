0d61cb1a221bc986f7142f49532bf6c3289e3ccc
Guillemj98 <guillemagro98@gmail.com>
2024-04-08 12:10:10 +0200

    Add actividad 18 calculadora JS

---
+++ 18_Deberes/js/operaciones.js
@@ -0,0 +1,25 @@
+function sumar(a,b){
+    let n1 = parseInt(a);
+    let n2 = parseInt(b);
+    let suma = n1+n2;
+    Resultado.value = suma;
+}
+function restar(a,b){
+    let n1 = parseInt(a);
+    let n2 = parseInt(b);
+    let resta = n1 - n2;
+    Resultado.value = resta;
+    
+}
+function dividir(a,b){
+    let n1= parseInt(a);
+    let n2= parseInt(b);
+    let dividir = n1 / n2;
+    Resultado.value = dividir;
+}
+function multiplicar(a,b){
+    let n1= parseInt(a);
+    let n2= parseInt(b);
+    let multiplicar = n1 * n2;
+    Resultado.value = multiplicar;
+}
