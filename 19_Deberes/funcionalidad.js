ed55940da9a158c399554e63db0a315049ad6caa
Guillemj98 <guillemagro98@gmail.com>
2024-04-15 11:50:16 +0200

    Ejercicio 19 de JS y HTML sin terminar

---
+++ 19_Deberes/funcionalidad.js
@@ -0,0 +1,26 @@
+// Contador de nuemero pares.
+
+
+// Conversor de temperaturas
+
+
+//Juego adivina el numero
+
+
+
+//Cambiar el color de la pantalla
+
+
+
+//Usuario y contraseña
+
+
+
+
+//Verificador de numeros primos
+
+
+
+
+
+//Generador de la secuencia de Fibonacci
